b96743ec88e1e634b694ac04f01bf8864dfb5546
Elyesarfa <elyesarfa7211@gmail.com>
2023-11-23 12:57:17 +0100

    printf

--- main.h
+++ main.h
@@ -1,14 +1,17 @@
-l#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdarg.h>
 
-typedef struct {
+typedef struct structname{
   char character;
   void (*f)(va_list);
 } ntype_t;
 
 int _printf(const char *format, ...);
 void errormsg();
+void pc(va_list l);
+void ppercent(va_list l);
+void ps(valist l);
 
 #endif
