d54a91f18d85fd8e7076a123d289d552b6bce386
elyessassi <mjoxen@gmail.com>
2023-11-23 12:59:03 +0100

    fonctions

---
+++ fonctions.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * 
+*/
+
+void pc(va_list l)
+{
+    putchar(va_arg(l, char));
+}
+
+/**
+ * 
+*/
+
+void ppercent(va_list l)
+{
+    putchar('%');
+    l = l;
+}
+
+/**
+ * 
+*/
+
+void ps(va_list l)
+{
+    char *str = va_arg(l, char *);
+    int i = 0;
+
+    while(str[i] == '\0')
+    {
+        putchar(str[i]);
+        i++;
+    }
+}
+
+/**
+ * 
+*/
+
+void errormsg()
+{
+    putchar('e');
+    putchar('r');
+    putchar('r');
+    putchar('o');
+    putchar('r');
+}
