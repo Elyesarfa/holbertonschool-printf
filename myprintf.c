6d380b43b399c4996acf887bb5f2b0fec78ba282
elyessassi <mjoxen@gmail.com>
2023-11-23 21:01:25 +0100

    files

---
+++ myprintf.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * 
+*/
+
+int _printf(const char *format, ...)
+{
+    int j = 0;
+    type_t fptr[] = {
+        {'c', pc}
+        {'s', ps}
+        {'%', ppercent}
+        {'\0', NULL}
+    }
+    
+    va_list list;
+    
+    va_start(list, format);
+    if (!format)
+        errormsg();
+        return (0);
+    int len = strlen(format);
+    while (j < len)
+    {
+        if (format[j] != '%')
+            putchar(format[j]);
+            j++;
+    }
+    va_end(list);
+    return (0);
+}
