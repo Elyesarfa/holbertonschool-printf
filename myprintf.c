22529e4202d4729c3dbb33253809abc4d7011448
elyessassi <mjoxen@gmail.com>
2023-11-24 08:13:32 +0100

    main printf

--- myprintf.c
+++ myprintf.c
@@ -9,63 +9,63 @@
 
 int _printf(const char *format, ...)
 {
-    int j = 0;
-    int i;
-    ntype_t fptr[] = {
-        {'c', pc},
-        {'s', ps},
-        {'%', ppercent},
-        {'\0', NULL}
-    };
-    int bytecount = 0;
-    int len;
-    va_list list;
-    void (*choose)(va_list);
-    
-    if (format == NULL)
-    {
-        return (-1);
-    }
-    len = strlen(format);
-    va_start(list, format);
-    while (j < len)
-    {
-        if (format[j] != '%')
-        {
-            if (format[j] == '\\' && format[j + 1] == 'n')
-            {
-                putchar('\n');
-                bytecount = bytecount + 2;
-                j = j + 2;
-            }
-            else
-            {
-                putchar(format[j]);
-                j++;
-                bytecount++;
-            }
-        }
-        else
-        {
-            for (i = 0 ; i < 3 ; i++)
-            {
-                if(format[j + 1] == fptr[i].character)
-                {
-                    choose = fptr[i].f;
-                    choose(list);
-                    j = j + 2;
-                    bytecount = bytecount + 2;
-                }
-                else
-                {
-                    putchar(format[j]);
-                    putchar(format[j + 1]);
-                    j = j + 2;
-                    bytecount = bytecount + 2;
-                }
-            }
-        }
-    }
-    va_end(list);
-    return (bytecount);
+	int j = 0;
+	int i;
+	ntype_t fptr[] = {
+		{'c', pc},
+		{'s', ps},
+		{'%', ppercent},
+		{'\0', NULL}
+	};
+	int bytecount = 0;
+	int len;
+	va_list list;
+	void (*choose)(va_list);
+	
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	len = strlen(format);
+	va_start(list, format);
+	while (j < len)
+	{
+		if (format[j] != '%')
+		{
+			if (format[j] == '\\' && format[j + 1] == 'n')
+			{
+				putchar('\n');
+				bytecount = bytecount + 2;
+				j = j + 2;
+			}
+			else
+			{
+				putchar(format[j]);
+				j++;
+				bytecount++;
+			}
+		}
+		else
+		{
+			for (i = 0 ; i < 3 ; i++)
+			{
+				if(format[j + 1] == fptr[i].character)
+				{
+					choose = fptr[i].f;
+					choose(list);
+					j = j + 2;
+					bytecount = bytecount + 2;
+				}
+				else
+				{
+					putchar(format[j]);
+					putchar(format[j + 1]);
+					j = j + 2;
+					bytecount = bytecount + 2;
+				}
+			}
+		}
+	}
+	va_end(list);
+	return (bytecount);
 }
