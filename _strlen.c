5cbf6c0a52f8d3092266c9425dc887f715074c8a
elyessassi <mjoxen@gmail.com>
2023-11-25 16:18:50 +0100

    betty

--- _strlen.c
+++ _strlen.c
@@ -2,17 +2,18 @@
 #include <stddef.h>
 
 /**
- *
+ *_strlen - count the number of character in a string
+ *@s: string
+ *Return: number of characters in the string
 */
 
 size_t _strlen(const char *s)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-
-    return len;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
