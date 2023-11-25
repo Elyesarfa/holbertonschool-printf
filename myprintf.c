6b4ca56d85cb5d78dae1394b5b095905047a1dd7
elyessassi <mjoxen@gmail.com>
2023-11-25 16:34:17 +0100

    betty

--- myprintf.c
+++ myprintf.c
@@ -4,36 +4,38 @@
 #include "main.h"
 
 /**
- * 
+ * _printf - main function
+ * @format: input
+ * Return: length of text printed
 */
 
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	va_list l;
-	va_start(l, format);
+int len = 0;
+va_list l;
 
+va_start(l, format);
 if (format == NULL)
 {
 	return (-1);
 }
 while (*format != '\0')
 {
-    if (*format == '%' && *(format + 1) != '\0')
+	if (*format == '%' && *(format + 1) != '\0')
 	{
 		format++;
-        len += format_specifier(*format, l);
+		len += format_specifier(*format, l);
 	}
-    else if (*format == '%' && *(format + 1) == '\0')
+	else if (*format == '%' && *(format + 1) == '\0')
 	{
 		return (-1);
 	}
 	else
-    {
+	{
 	_putchar(*format);
 	len++;
-    }
-    format++;
+	}
+	format++;
 }
 va_end(l);
 return (len);
